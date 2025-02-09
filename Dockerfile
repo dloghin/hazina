ce7ec21457b6f9aa58efbef479331b583849a9b4
Dumi Loghin <dumiloghin@gmail.com>
2025-02-09 19:38:47 +0800

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.12-slim
 
-RUN apt update && apt install -y git
+RUN apt update && apt install -y git gcc
 RUN cd / && git clone https://github.com/dloghin/hazina.git
 WORKDIR /hazina
 RUN pip install --upgrade pip && pip install -r requirements.txt
--- app/trader_server.py
+++ app/trader_server.py
@@ -2,8 +2,10 @@ import asyncio
 from threading import Lock, Thread
 from flask import Flask, json, request
 from trader import run_trader_ollama, run_trader_openai, stop_trader
+from flask_executor import Executor
 
 api = Flask(__name__)
+executor = Executor(api)
 
 lock = Lock()
 messages = []
@@ -11,30 +13,34 @@ messages = []
 def save_message(message):
     global messages
     global lock
-    with lock:
-      messages.append(message)
+    # with lock:
+    messages.append(message)
     print(message)
 
 def pop_messages():
     global messages
     ret = json.dumps(messages)
-    with lock:
-      messages = []
+    # with lock:
+    messages = []
     return ret
 
 def run_trader(provider):
     if provider == "ollama":
+      print("run trader with ollama")
       asyncio.run(run_trader_ollama(save_message))
     else:
+      print("run trader with openai")
       asyncio.run(run_trader_openai(save_message))
+    print("run trader done")
 
 @api.route('/start', methods=['POST'])
 def start_trader_ep():
     provider = request.args['model']
     if provider not in ["ollama", "openai"]:
        return json.dumps({"success": False, "error": "unknown models provider"}), 201
-    task = Thread(target=run_trader, args=[provider])
-    task.start()
+    #task = Thread(target=run_trader, args=[provider])
+    #task.start()
+    executor.submit(run_trader, provider)
     return json.dumps({"success": True}), 201
 
 @api.route('/stop', methods=['POST'])
